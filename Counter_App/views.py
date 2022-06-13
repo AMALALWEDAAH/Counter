c61aa587c1840088f24b8d7f264eea74c2c31840
Amal <apptalid@gmail.com>
2022-06-13 08:25:01 +0300

    first commit

---
+++ Counter_App/views.py
@@ -0,0 +1,27 @@
+from multiprocessing import context
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, HttpResponse, redirect
+def index(request):
+    return HttpResponse("this is the equivalent of @app.route('/')!")
+
+
+
+def visitnumber(request):
+    visitnumber=request.session.get('visitnumber',0)
+    request.session['visitnumber'] = visitnumber + 1
+    context={
+        'visitnumber':visitnumber,
+    }
+
+
+    return render(request, 'index.html', context=context)
+
+def destroySession (request):
+    try:
+        del request.session['visitnumber']
+    except  KeyError:
+        pass
+    return redirect ('/')
+
